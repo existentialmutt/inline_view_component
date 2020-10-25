ca8f368ef0535e8f3525bcd7bcf81559009b7a3b
Rafe Rosen <rrosen@greenriver.org>
2020-10-24 21:31:25 -0400

    set call when templates is defined

--- lib/inline_view_component.rb
+++ lib/inline_view_component.rb
@@ -1,7 +1,6 @@
-require "inline_view_component/railtie"
 require "active_support/concern"
 require "tilt"
-require "active_support/core_ext/string/output_safety"
+# require "active_support/core_ext/string/output_safety"
 
 module InlineViewComponent
   extend ActiveSupport::Concern
@@ -14,7 +13,6 @@ module InlineViewComponent
     end
 
     # set the template format for this component
-
     def self.inline_template_format=(format)
       unless InlineViewComponent::FORMATS.include? format
         raise "unsupported format #{format}.  Valid choices are: #{InlineViewComponent::FORMATS}"
@@ -25,21 +23,28 @@ module InlineViewComponent
 
     # set the template for this component
     def self.template(template_string)
-      format = inline_template_format || :erb # Rails.configuration.x.inline_view_component.default_template_format
+      format = inline_template_format || :erb
 
-      raise "unsupported format #{format}" unless FORMATS.include?(format)
+      @inline_template = InlineViewComponent::Compiler.compile_template(format, template_string)
 
-      @inline_template = case format.to_sym
-        when :erb then Tilt["erubi"].new(nil, 1, escape: true) { template_string }
-          # when :erb then Tilt::ErubiTemplate.new(nil, bufval: "ActiveSupport::SafeBuffer.new", escapefunc: "ERB::Util::h", escape: true) { template_string }
-        when :haml then Tilt["haml"].new(nil, 1, escape_html: true, format: :html5) { template_string }
+      define_method :call do
+        raw self.class.inline_template.render(self)
+      end
+    end
+  end
+
+  module Compiler
+    def compile_template(format, template_string)
+      raise "unsupported format #{format}" unless InlineViewComponent::FORMATS.include?(format)
+
+      case format.to_sym
+      when :erb then Tilt["erubi"].new(nil, 1, escape: true) { template_string }
+        # TODO maybe something like this would get html_safe working
+        # when :erb then Tilt::ErubiTemplate.new(nil, bufval: "ActiveSupport::SafeBuffer.new", escapefunc: "ERB::Util::h", escape: true) { template_string }
+      when :haml then Tilt["haml"].new(nil, 1, escape_html: true, format: :html5) { template_string }
       end
     end
 
-    def call
-      raise "No template defined" unless self.class.inline_template
-
-      raw self.class.inline_template.render(self)
-    end
+    module_function :compile_template
   end
 end
--- lib/inline_view_component/railtie.rb
+++
@@ -1,5 +0,0 @@
-module InlineViewComponent
-  class Railtie < ::Rails::Railtie
-    # config.x.inline_view_component.default_template_format = :erb
-  end
-end
